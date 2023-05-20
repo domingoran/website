9e74ec5b47e8f70ef73cbd4d4ef68e0291e43382
domingoran <domiran98@gmail.com>
2023-05-21 00:12:54 +0200

    title mod

--- scripts/logo.js
+++ scripts/logo.js
@@ -6,7 +6,7 @@ function complete() {
 
 
 const listText = ['R4n+Dom', 'R4nDoM', ' r4nDoM', ' r4n!D0M', 'r4nd0M', 'r4Nd0M',
-    'R4N?d0M', 'R4ND0M', 'R4ND0m', 'RaN/D0m', 'RaNDom',];
+    'R4N"d0M"', 'R4ND0M', 'R4ND0m', 'RaN/D0m', 'RaNDom',];
 
 let idx = 0;
 let timer = setInterval(typeWrite, 800);
