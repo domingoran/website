09db7a7b343959de54ae7112afc613692e23468d
domingoran <domiran98@gmail.com>
2026-06-27 11:04:43 +0200

    updates

--- scripts/about.js
+++ scripts/about.js
@@ -18,17 +18,30 @@ function appendData(data) {
     n = getRndInteger(0, data.length)
     {
         let text = document.getElementById("pl");
+        let title = data[n].title.replace(/\n/g, '<br/>');
+        let icons = '';
+        for (let j = 0; j < data[n].icons.length; j++) {
+            icons +=
+                '<a href="' + data[n].links[j] + '" class="icon">' +
+                '<i class="' + data[n].icons[j] + '"></i>' +
+                '</a>';
+        }
         text.innerHTML += '<h4> This is one of my projects, take a look!</h4>' +
             '<div class="portfolio-item">' +
+            '<div class="box1">' +
             '<div class="image">' +
-            '<img src= "' + (data[n].image) + '" alt=" ' + (data[n].alt) + '">' +
+            '<img src="' + data[n].image + '" alt="' + data[n].alt + '">' +
             '</div>' +
             '<div class="hover-items">' +
-            "<h3>" + data[n].title + "</h3>" +
-            '<div class="icons">' +
-            '<a href= "' + (data[n].links)[0] + '"class="icon">' +
-            '<i class=" ' + (data[n].icons)[0] + '"></i>' +
-            '</a>' +
+            '<h3>' + title + '</h3>' +
+            '<div class="icons">' + icons + '</div>' +
+            '</div>' +
+            '</div>' +
+            '<div class="box2">' +
+            '<div class="portfolio-text">' +
+            '<h5>' + data[n].genre + '</h5>' +
+            '<h4>' + title + '</h4>' +
+            '<p>' + data[n].paragraph + '</p>' +
             '</div>' +
             '</div>' +
             '</div>'
